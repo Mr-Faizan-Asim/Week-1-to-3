47a65861f34e940e8934d3032b91c751a662c887
Mr-Faizan-Asim <120254196+Mr-Faizan-Asim@users.noreply.github.com>
2022-12-22 07:44:10 +0500

    Add files via upload

---
+++ Week 1/aeroplane.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+main()
+{
+
+
+cout<<"             |             "<< endl;
+cout<<"            / \\             "<< endl;
+cout<<"           /   \\             "<< endl;
+cout<<"          /     \\             "<< endl;
+cout<<"          |     |          "<< endl;
+cout<<"          |     |          "<< endl;
+cout<<"          |     |          "<< endl;
+cout<<"         .|  |  |.          "<< endl;
+cout<<"        / |  |  | \\          "<< endl;
+cout<<"       |  |  |  |  |          "<< endl; 
+cout<<"       | _|__|__|_ |          "<< endl;
+cout<<"       |/    |    \\|          "<< endl;
+
+
+
+
+}
